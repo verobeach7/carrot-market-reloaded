cedc36215f413fd36e3e46c93d6ad52eb1c96173
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-06-07 22:05:23 +0900

    Carrot Market: Tailwind(className Variables: ring --tw-variable)

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,20 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm dark:bg-gray-600">
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col">
-            <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">
-              In transit
-            </span>
-            <span className="text-4xl font-semibold dark:text-white">
-              Cool Blue
-            </span>
-          </div>
-          <div className="size-12 rounded-full bg-orange-500" />
-        </div>
-        <div className="my-2 flex items-center gap-2">
-          <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full transition hover:bg-green-500 hover:scale-125">
-            Today
-          </span>
-          <span className="dark:text-gray-100">9:30-10:30</span>
-        </div>
-        <div className="relative">
-          <div className="absolute bg-gray-200 w-full rounded-full h-2" />
-          <div className="absolute bg-green-400 w-2/3 rounded-full h-2" />
-        </div>
-        <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span className="text-gray-400 dark:text-gray-500">Delivered</span>
-        </div>
+    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 ">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2">
+        <input
+          // input의 높이를 조정하기 위해서 두가지 방식 사용 가능
+          // h-10을 이용 또는 py-3을 이용(높이 또는 패딩을 이용하여 조정)
+          /* className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-orange-500 ring-offset-2 ring-offset-blue-600" */
+          className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus: ring-offset-2 transition-shadow placeholder:drop-shadow"
+          // ring은 outline과 비슷하지만 ring은 커스텀이 가능함!!!
+          type="text"
+          placeholder="Search here..."
+        />
+        {/* bg-black은 --tw-bg-opacity를 변수로 가지고 있어 bg-opacity를 설정하는 경우 이 값을 이용하여 bg-black을 계산함 */}
+        <button className="bg-black bg-opacity-50 text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none">
+          Search
+        </button>
       </div>
     </main>
   );
