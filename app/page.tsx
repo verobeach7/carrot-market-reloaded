75cde47f14ac900aaa00af716774f1a88070d999
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-06-09 15:47:14 +0900

    Carrot Market: Tailwind(Directives: base, components, utilities, layer, apply)

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ export default function Home() {
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5 ">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
         <input />
-        <button className="w-full bg-black h-10 text-white rounded-sexy-name mt-tomato">
-          Submit
-        </button>
+        <a href="#">aaaaa</a>
+        <button className="btn">Submit</button>
       </div>
     </main>
   );
