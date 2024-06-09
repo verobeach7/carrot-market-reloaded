a52db054c4cc1077ed09c58e2e7f2e920869701e
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-06-09 17:04:39 +0900

    Carrot Market: Authentication UI(Home Screen)

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-neutral-900 text-white max-w-sm mx-auto`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
