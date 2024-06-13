bd55085ff46a7b53b7c71ed8c290fd49ccdcf921
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-06-13 20:09:17 +0900

    Carrot Market: Server Actions(don't need to use useState, onChange, fetch)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,15 @@
-"use client";
-
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function LogIn() {
-  const onClick = async () => {
-    const response = await fetch("/www/users", {
-      method: "POST",
-      body: JSON.stringify({
-        username: "nico",
-        password: "1234",
-      }),
-    });
-    console.log(await response.json());
+  /* Server Actions */
+  // async function handleForm()=>{
+  const handleForm = async (formData: FormData) => {
+    "use server";
+    console.log(formData.get("email"), formData.get("password"));
+    // 함수가 서버에서만 실행되도록 만들어 줌
+    console.log("I run on the server.");
   };
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
@@ -21,18 +17,23 @@ export default function LogIn() {
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[]} />
+      <form action={handleForm} className="flex flex-col gap-3">
         <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={[]}
+        />
+        <FormInput
+          name="password"
           type="password"
           placeholder="Password"
           required
           errors={[]}
         />
-      </form>
-      <span onClick={onClick}>
         <FormButton loading={false} text="Login" />
-      </span>
+      </form>
       <SocialLogin />
     </div>
   );
