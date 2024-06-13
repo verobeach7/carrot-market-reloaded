75cc44d83d498cbdb460cb64dc9f0f1e44249282
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-06-13 21:37:44 +0900

    Carrot Market: Server Actions(useFormState: result of action)

---
+++ app/login/actions.ts
@@ -0,0 +1,10 @@
+"use server";
+
+/* Server Actions */
+export const handleForm = async (prevState: any, formData: FormData) => {
+  console.log(prevState);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  return {
+    errors: ["wrong password", "password too short"],
+  };
+};
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,22 @@
+"use client";
+
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
+import { useFormState } from "react-dom";
+import { handleForm } from "./actions";
 
 export default function LogIn() {
-  /* Server Actions */
-  const handleForm = async (formData: FormData) => {
-    "use server";
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    console.log("logged in!");
-  };
+  // useFormState는 결과를 알고싶은 action을 인자로 넘겨줘야 함
+  // initialValue를 보통 null로 설정함
+  const [state, action] = useFormState(handleForm, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password</h2>
       </div>
-      <form action={handleForm} className="flex flex-col gap-3">
+      <form action={action} className="flex flex-col gap-3">
         <FormInput
           name="email"
           type="email"
@@ -28,7 +29,9 @@ export default function LogIn() {
           type="password"
           placeholder="Password"
           required
-          errors={[]}
+          // state가 초기에 null로 되어있기 때문에 없을 수도 있음 -> Optional로 설정
+          // state에 error가 없다면 빈 배열로 대체
+          errors={state?.errors ?? []}
         />
         <FormButton text="Login" />
       </form>
