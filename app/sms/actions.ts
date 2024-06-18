554907b3b8fe37d7adf0137efc496a6c29b3cb40
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-06-18 16:36:04 +0900

    Carrot Market: Validation(SMS Validation: use prevState, typescript)

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -2,13 +2,48 @@
 
 import { z } from "zod";
 import validator from "validator";
+import { redirect } from "next/navigation";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "Wrong phone format."
+  );
 
 // coerce를 이용해 입력받은 값을 원하는 type으로 바꿔줄 수 있음
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export const smsLogin = async (prevState: any, formData: FormData) => {
-  console.log(typeof formData.get("token"));
-  console.log(typeof tokenSchema.parse(formData.get("token")));
+interface ActionState {
+  token: boolean;
+}
+
+export const smsLogin = async (prevState: ActionState, formData: FormData) => {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      return {
+        token: false,
+        error: result.error.flatten(),
+      };
+    } else {
+      return {
+        token: true,
+      };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      console.log(result.error.flatten());
+      return {
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      redirect("/");
+    }
+  }
 };
