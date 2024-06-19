13e8de4b79a0f7650ae26916a6f55b23e2020b17
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-06-19 23:20:52 +0900

    Carrot Market: Prisma(Relationships: connect, include)

--- lib/db.ts
+++ lib/db.ts
@@ -2,4 +2,28 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
+async function test() {
+  /* const token = await db.sMSToken.create({
+    data: {
+      token: "121212",
+      user: {
+        connect: {
+          id: 1,
+        },
+      },
+    },
+  }); */
+  const token = await db.sMSToken.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      user: true,
+    },
+  });
+  console.log(token);
+}
+
+test();
+
 export default db;
