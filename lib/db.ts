4cae6f51abaad69b96a1be0f5737e7880ccd65cb
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-06-19 22:32:27 +0900

    Carrot Market: Prisma(Prisma Studio)

--- lib/db.ts
+++ lib/db.ts
@@ -2,22 +2,4 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
-async function test() {
-  /* const user = await db.user.create({
-    data: {
-      username: "test",
-    },
-  }); */
-  const users = await db.user.findMany({
-    where: {
-      username: {
-        contains: "est",
-      },
-    },
-  });
-  console.log(users);
-}
-
-test();
-
 export default db;
