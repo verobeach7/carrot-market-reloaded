3e5d4ad751c6c3db828d524a518e5c55df56d4e2
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-06-29 21:40:40 +0900

    Carrot Market: Authentication(Matcher: Regex OR if-else)

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,19 @@
-import { cookies } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
-import getSession from "./lib/session";
+import { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  // console.log(request.cookies.getAll());
-  console.log(cookies());
-  const session = await getSession();
-  console.log(session);
-  if (request.nextUrl.pathname === "/profile") {
-    /* return Response.json({
-      error: "you are not allowed here!",
-    }); */
-    // return Response.redirect(new URL("/", request.url));
-    // NextResponse는 Response의 확장형
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  console.log("hello");
 }
+
+export const config = {
+  //   matcher: ["/", "/profile", "/create-account", "/user/:path*"],
+  /*
+   * Match all request paths except for the ones starting with:
+   * - api (API routes)
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - favicon.ico (favicon file)
+   */
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
