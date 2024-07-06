7cc8d3c751501cd007e4372bc2a3c47c544b422b
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-07-06 15:42:52 +0900

    Carrot Market: Products(Tab Bar: CSS, usePathname method, use client)

--- components/tab-bar.tsx
+++ components/tab-bar.tsx
@@ -1,27 +1,67 @@
-import { HomeIcon } from "@heroicons/react/24/solid";
+"use client";
+
+// HomeIcon을 solid와 outline으로 import할 때 이름이 같아 불가능함. Alias(as)처리를 하여 이름을 구분해줘야 함
+import {
+  NewspaperIcon as OutlineNewspaperIcon,
+  HomeIcon as OutlineHomeIcon,
+  ChatBubbleOvalLeftEllipsisIcon as OutlineChatIcon,
+  VideoCameraIcon as OutlineLiveIcon,
+  UserIcon as OutlineUserIcon,
+} from "@heroicons/react/24/outline";
+import {
+  NewspaperIcon as SolidNewspaperIcon,
+  HomeIcon as SolidHomeIcon,
+  ChatBubbleOvalLeftEllipsisIcon as SolidChatIcon,
+  VideoCameraIcon as SolidLiveIcon,
+  UserIcon as SolidUserIcon,
+} from "@heroicons/react/24/solid";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function TabBar() {
+  // usePathname()은 url 경로를 가져옴, Interactive한 요소로 "use client"처리 해줘야 함
+  const pathName = usePathname();
   return (
-    <div>
-      <Link href="/products" className="">
-        <HomeIcon className="w-7 h-7" />
+    // *:text-white: div 안의 모든 elements가 하얀색 text를 가지게 됨
+    <div className="fixed bottom-0 w-full mx-auto max-w-screen-md grid grid-cols-5 border-neutral-600 border-t px-5 py-3 *:text-white">
+      <Link href="/products" className="flex flex-col items-center gap-px">
+        {pathName === "/products" ? (
+          <SolidHomeIcon className="w-7 h-7" />
+        ) : (
+          <OutlineHomeIcon className="w-7 h-7" />
+        )}
         <span>홈</span>
       </Link>
-      <Link href="/life" className="">
-        <HomeIcon className="w-7 h-7" />
+      <Link href="/life" className="flex flex-col items-center gap-px">
+        {pathName === "/life" ? (
+          <SolidNewspaperIcon className="w-7 h-7" />
+        ) : (
+          <OutlineNewspaperIcon className="w-7 h-7" />
+        )}
         <span>동네생활</span>
       </Link>
-      <Link href="/chat" className="">
-        <HomeIcon className="w-7 h-7" />
+      <Link href="/chat" className="flex flex-col items-center gap-px">
+        {pathName === "/chat" ? (
+          <SolidChatIcon className="w-7 h-7" />
+        ) : (
+          <OutlineChatIcon className="w-7 h-7" />
+        )}
         <span>채팅</span>
       </Link>
-      <Link href="/live" className="">
-        <HomeIcon className="w-7 h-7" />
+      <Link href="/live" className="flex flex-col items-center gap-px">
+        {pathName === "/live" ? (
+          <SolidLiveIcon className="w-7 h-7" />
+        ) : (
+          <OutlineLiveIcon className="w-7 h-7" />
+        )}
         <span>쇼핑</span>
       </Link>
-      <Link href="/profile" className="">
-        <HomeIcon className="w-7 h-7" />
+      <Link href="/profile" className="flex flex-col items-center gap-px">
+        {pathName === "/profile" ? (
+          <SolidUserIcon className="w-7 h-7" />
+        ) : (
+          <OutlineUserIcon className="w-7 h-7" />
+        )}
         <span>나의 당근</span>
       </Link>
     </div>
