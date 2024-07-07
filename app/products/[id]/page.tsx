34b93bc438cd03edae669d8c38ea53f6d92f3998
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-07-07 17:51:33 +0900

    Carrot Market: Products(Product Detail Screen: Tailwind CSS, DB)

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,12 +1,93 @@
-async function getProduct() {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { formatToWon } from "@/lib/utils";
+import { UserIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+async function getIsOwner(userId: number) {
+  const session = await getSession();
+  // 로그인되어 있다면 로그인id와 product의 userId가 같은지 확인(소유자 확인)
+  if (session.id) {
+    // 소유자이면 true, 소유자가 아니면 false 반환
+    return session.id === userId;
+  }
+  // 로그인되어 있지 않다면 false 반환
+  return false;
+}
+
+async function getProduct(id: number) {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      user: {
+        select: {
+          username: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  return product;
 }
 
 export default async function ProductDetail({
-  params: { id },
+  params,
 }: {
   params: { id: string };
 }) {
-  const product = await getProduct();
-  return <span>Product detail of the id {id}</span>;
+  // id에 문자열 string이 들어오는 경우 오류처리
+  const id = Number(params.id);
+  if (isNaN(id)) {
+    return notFound();
+  }
+  const product = await getProduct(id);
+  // db에 없는 product id이면 notFound 페이지 보여주기
+  if (!product) {
+    return notFound();
+  }
+  // 소유자인지 확인
+  const isOwner = await getIsOwner(product.userId);
+  return (
+    <div>
+      <div className="relative aspect-square">
+        <Image fill src={product.photo} alt={product.title} />
+      </div>
+      <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
+        <div className="size-10 rounded-full">
+          {product.user.avatar !== null ? (
+            <Image
+              src={product.user.avatar}
+              width={40}
+              height={40}
+              alt={product.user.username}
+            />
+          ) : (
+            <UserIcon className="size-10" />
+          )}
+        </div>
+        <div>
+          <h3>{product.user.username}</h3>
+        </div>
+      </div>
+      <div className="p-5">
+        <h1 className="text-2xl font-semibold">{product.title}</h1>
+        <p>{product.description}</p>
+      </div>
+      <div className="fixed w-full bottom-0 left-0 p-5 pb-10 bg-neutral-800 flex justify-between items-center">
+        <span className="font-semibold text-xl">
+          {formatToWon(product.price)}원
+        </span>
+        <Link
+          className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold"
+          href={``}
+        >
+          채팅하기
+        </Link>
+      </div>
+    </div>
+  );
 }
