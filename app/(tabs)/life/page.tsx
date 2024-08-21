ff9c25b49c795e2762b8ec94f154719626259cd1
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-08-21 23:17:59 +0900

    Carrot Market: Optimistic updates(See Posts: Loading skeleton, DB _count, tailwindCSS)

--- app/(tabs)/life/page.tsx
+++ app/(tabs)/life/page.tsx
@@ -1,7 +1,68 @@
-export default function Life() {
+import db from "@/lib/db";
+import { formatToTimeAgo } from "@/lib/utils";
+import {
+  ChatBubbleBottomCenterIcon,
+  HandThumbUpIcon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { title } from "process";
+
+// loading.tsx파일에 skeleton을 구현하고 확인하기 위해 무한로딩 실행
+async function getPosts() {
+  const posts = db.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      views: true,
+      created_at: true,
+      _count: {
+        select: {
+          comments: true,
+          likes: true,
+        },
+      },
+    },
+  });
+  return posts;
+}
+
+export const metadata = {
+  title: "동네생활",
+};
+
+export default async function Life() {
+  const posts = await getPosts();
   return (
-    <div>
-      <h1 className="text-white text-4xl">Life!</h1>
+    <div className="p-5 flex flex-col">
+      {posts.map((post) => (
+        <Link
+          key={post.id}
+          href={`/posts/${post.id}`}
+          className="pb-5 mb-5 border-b border-neutral-500 text-neutral-400 flex flex-col gap-2 last:pb-0 last:border-b-0"
+        >
+          <h2 className="text-white text-lg font-semibold">{post.title}</h2>
+          <p>{post.description}</p>
+          <div className="flex items-center justify-between text-sm">
+            <div className="flex gap-4 items-center">
+              <span>{formatToTimeAgo(post.created_at.toString())}</span>
+              <span>・</span>
+              <span>조회 {post.views}</span>
+            </div>
+            {/* *:을 이용하여 자식 모두에게 적용할 수 있음 */}
+            <div className="flex gap-4 items-center *:flex *:gap-1 *:items-center">
+              <span>
+                <HandThumbUpIcon className="size-4" />
+                {post._count.likes}
+              </span>
+              <span>
+                <ChatBubbleBottomCenterIcon className="size-4" />
+                {post._count.comments}
+              </span>
+            </div>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
