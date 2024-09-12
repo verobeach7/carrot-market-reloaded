d76b107196072c0e50618e8eb61c2544f01f5911
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-09-12 21:01:24 +0900

    Carrot Market: Realtime Chat(Realtime Channel: Supabase, createClient, channel)

--- components/chat-messages-list.tsx
+++ components/chat-messages-list.tsx
@@ -6,17 +6,24 @@
 import { InitialChatMessages } from "@/app/chats/[id]/page";
 import { formatToTimeAgo } from "@/lib/utils";
 import { ArrowUpCircleIcon } from "@heroicons/react/24/solid";
+import { createClient } from "@supabase/supabase-js";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+// Step 1. Supabase url & public key
+const SUPABASE_URL = "https://ludtcgrfltawhjqshotg.supabase.co";
+const SUPABASE_PUBLIC_KEY = process.env.SUPABASE_PUBLIC_KEY;
 
 interface IChatMessageListProps {
   initialMessages: InitialChatMessages;
   userId: number;
+  chatRoomId: string;
 }
 
 export default function ChatMessagesList({
   initialMessages,
   userId,
+  chatRoomId,
 }: IChatMessageListProps) {
   const [messages, setMessages] = useState(initialMessages);
   const [message, setMessage] = useState("");
@@ -28,9 +35,34 @@ export default function ChatMessagesList({
   };
   const onSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    alert(message);
+    setMessages((prevMsgs) => [
+      ...prevMsgs,
+      {
+        // DB로부터 id를 받은 것이 아니기 때문에 fake id를 만들어 사용
+        id: Date.now(),
+        payload: message,
+        created_at: new Date(),
+        userId,
+        user: {
+          username: "string",
+          avatar: "xxx",
+        },
+      },
+    ]);
     setMessage("");
   };
+
+  // 시작할 때만 useEffect가 작동하도록 함
+  useEffect(() => {
+    // Step 2. create supabase client
+    const client = createClient(SUPABASE_URL, SUPABASE_PUBLIC_KEY!);
+    // 채팅방은 아무나 접근할 수 없어야 하기 때문에 고유하면서 아무도 추측할 수 없는 랜덤string으로 해야함
+    const channel = client.channel(`room-${chatRoomId}`);
+    channel.on("broadcast", { event: "message" }, (payload) => {
+      console.log(payload);
+    });
+  }, []);
+
   return (
     // 메시지를 아래서부터 채워지게 하기 위해서 스크린 최소 사이즈를 정해주고 아래쪽부터 채워주게 함
     <div className="p-5 flex flex-col gap-5 min-h-screen justify-end">
