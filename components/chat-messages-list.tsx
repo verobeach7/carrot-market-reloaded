f413556beca1b12ed69cc5c26ebe03e716c77cb6
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-09-14 11:38:34 +0900

    Carrot Market: Realtime Chat(Save Message: actions.ts, db)

--- components/chat-messages-list.tsx
+++ components/chat-messages-list.tsx
@@ -4,6 +4,7 @@
 // 새 message가 들어오면 state에 추가
 
 import { InitialChatMessages } from "@/app/chats/[id]/page";
+import { saveMessage } from "@/app/chats/actions";
 import { formatToTimeAgo } from "@/lib/utils";
 import { ArrowUpCircleIcon } from "@heroicons/react/24/solid";
 import { createClient, RealtimeChannel } from "@supabase/supabase-js";
@@ -51,7 +52,7 @@ export default function ChatMessagesList({
     } = event;
     setMessage(value);
   };
-  const onSubmit = (event: React.FormEvent) => {
+  const onSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     setMessages((prevMsgs) => [
       ...prevMsgs,
@@ -82,6 +83,7 @@ export default function ChatMessagesList({
         },
       },
     });
+    await saveMessage(message, chatRoomId);
     setMessage("");
   };
 
