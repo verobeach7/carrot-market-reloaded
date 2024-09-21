ff28071abfdbce4ae3a47b5eb8541c9194bd13c1
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-09-21 21:41:00 +0900

    Carrot Market: Extras(Private Folders: _)

---
+++ app/_private_folder/page.tsx
@@ -0,0 +1,10 @@
+export default function PrivateFolder() {
+  // tailwind.config.ts에서 만든 className을 활용하여 폰트 적용
+  return (
+    <div className="flex flex-col gap-3 py-10">
+      <h1 className="text-6xl font-rubik">Extras!</h1>
+      <h1 className="text-6xl font-metallica">Extras!</h1>
+      <h2 className="font-roboto">So much more to learn!</h2>
+    </div>
+  );
+}
